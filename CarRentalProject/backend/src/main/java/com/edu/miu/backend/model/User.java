76c25361bd8746e2afdfb4c97c3c982766eb16b8
LEMESSA ADUGNA <abdibori52@gmail.com>
2023-02-11 20:23:24 -0600

    back end commited again1

--- CarRentalProject/backend/src/main/java/com/edu/miu/backend/model/User.java
+++ CarRentalProject/backend/src/main/java/com/edu/miu/backend/model/User.java
@@ -21,7 +21,7 @@ import java.util.Set;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
     @JsonIgnore
